9c7264647e63316a2c78d5f920d39f7ccae7be47
JinHyeongLee <rute92@naver.com>
2019-12-17 09:37:09 +0900

    img_send edit

--- darknet/src/Linux/img_send.cpp
+++ darknet/src/Linux/img_send.cpp
@@ -131,135 +131,21 @@ int main(int argc, char** argv)
   		recv_time -= current_time;
   		cout << "time: " << recv_time << endl;
 #endif
-		if (recv_buff[0] == 't') {
-			ret = fwrite(recv_buff, 1, 2, fp);
-			if(ret < 0) {
-				cerr << "write error\n";
-				return -1;
-			}
-			fseek(fp, 0, SEEK_SET);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+		ret = fwrite(recv_buff, 1, 2, fp);
+		if(ret < 0) {
+			cerr << "write error\n";
+			return -1;
 		}
+		fseek(fp, 0, SEEK_SET);
 #ifdef DEBUG
-		cout << "recv: " << recv_buff[0] << recv_buff[1] << endl;
+		cout << "recv: " << recv_buff[0] << recv_buff[1] << recv_buff[2] << endl;
 #endif
 	}
 	
-
-// 	h = img.rows;
-// 	w = img.cols;
-// 	c = img.channels();
-// 	step = img.step;
-
-// 	send_info[0] = h;
-// 	send_info[1] = w;
-// 	send_info[2] = c;
-// 	total_size = h*w*c;
-// #ifdef DEBUG
-// 	printf("[img] rows: %d, cols: %d, channels: %d, step: %d\n", h, w, c, step);
-// #endif
-
-// 	// 서버에 connect 요청
-// 	if( -1 == connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr))) {
-// 		cerr << "connect fail\n";
-// 		return -1;
-// 	}
-// #ifdef DEBUG
-// 	printf("connect ok\n");IMREAD_COLOR
-// #endif
-
-// 	// image info 전송 (h, w, c)
-// 	ret = send(client_socket, send_info, sizeof(send_info), 0);
-// 	//ret = sendto(client_socket, send_info, sizeof(send_info), 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
-// 	if(ret < 0) {
-// 		cerr << "image info send fail\n";
-// 		return -1;
-// 	}
-
-// 	// Mat data 담을 buff 생성
-// 	send_data = (unsigned char*)calloc(total_size, sizeof(unsigned char));
-// 	//send_data = (float*)calloc(total_size, sizeof(float));
-	
-// 	while(1)
-// 	{
-// #ifdef DEBUG
-
-// #endif
-// 		cap.read(img);
-// 		if(img.empty()){
-// 			cerr << "image is empty\n";
-// 			return -1;
-// 		}
-// 		namedWindow("cam", WINDOW_AUTOSIZE);
-// 		imshow("cam", img);
-// 		waitKey(1);
-// 		// buff에 Mat data 복사
-// 		for(i = 0; i < h; ++i){
-// 			for(k = 0; k < c; ++k){
-// 				for(j = 0; j < w; ++j){
-// 					send_data[k*w*h + i*w + j] = (unsigned char)img.data[i*step + j*c + k];
-// 					//send_data[k*w*h + i*w + j] = img.data[i*step + j*c + k]/255.0f;
-// 				}
-// 			}
-// 		}
-		
-// 		struct timeval time;
-// 		gettimeofday(&time, NULL);
-// 		double current_time = (double)time.tv_sec + (double)time.tv_usec * .000001;
-
-// 		for(i = 0; data_loc < total_size; i++) {
-// 		//for(i = 0; data_loc < total_size*4; i++) {
-// 			ret = send(client_socket, send_data + data_loc, SEND_SIZE, 0);
-// 			//ret = sendto(client_socket, send_data + data_loc, SEND_SIZE, 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
-// 			if(ret < 0) {
-// 				cerr << "image data send fail\n";
-// 				return -1;
-// 			}
-// 			data_loc += SEND_SIZE;
-// 		}
-// 		data_loc = 0;
-
-// 		gettimeofday(&time, NULL);
-//  		double recv_time1 = (double)time.tv_sec + (double)time.tv_usec * .000001;
-//  		recv_time1 -= current_time;
-//  		cout << "time: " << recv_time1 << endl;
-
-// #ifdef DEBUG
-// 		cerr << "i : " << i << endl;
-// 		cerr << "write ret : " << ret << endl;
-// 		cerr << "[send img] size: " << total_size << endl;
-		
-
-// #endif
-		// 서버 신호 대기
-// 		ret = recv(client_socket, recv_buff, sizeof(recv_buff), 0);
-// 		if(ret < 0) {
-// 			cerr << "[img_send.cpp] message read fail\n";
-// 			return -1;
-// 		}
-
-// 		if(recv_buff[0] == 'c') break;
-// 		else if(recv_buff[0] == 'o') {
-// #ifdef DEBUG
-// 			cerr << "read ret : " << ret << endl;
-// #endif
-// 		}
-// 		else if(recv_buff[0] == 't') {
-// 			// tracking mode command
-// 			cout << "[tracking command]: " << recv_buff[1] << endl;
-// 		}
-
-// #ifdef DEBUG
-//  		cout << "count: "<< count++ << endl;
-		
-//  		gettimeofday(&time, NULL);
-//  		double recv_time = (double)time.tv_sec + (double)time.tv_usec * .000001;
-//  		recv_time -= current_time;
-//  		cout << "time: " << recv_time << endl;
-// #endif
-//	}
 	cerr << "Quit the program!\n";
 #ifdef DEBUG
-	destroyWindow("cam");
+	destroyWindow("img");
 #endif
 	free(send_data);
 	fclose(fp);
