db6184245456fdcd932c7b9a006435822075bed1
Jinhyeong Lee <rute92@naver.com>
2019-12-18 19:45:53 -0800

    img_send 코드 수정

--- darknet/src/Linux/img_send.cpp
+++ darknet/src/Linux/img_send.cpp
@@ -17,6 +17,9 @@
 //#define SERVER_IP	"192.168.0.38" // my pc
 //#define SERVER_IP	"127.0.0.1"
 #define SEND_SIZE	1024 //58368
+#define MODE_FILE_NAME    "~/web/mode.txt"
+#define MODE110_FILE_NAME    "~/mode110.txt"
+#define MODE310_FILE_NAME    "~/mode310.txt"
 #define DEBUG
 
 using namespace cv;
@@ -24,6 +27,8 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+
+    FILE* mode_fp, mode110_fp, mode310_fp;
 	int client_socket;
 	struct sockaddr_in server_addr;
 
@@ -76,8 +81,12 @@ int main(int argc, char** argv)
 		cerr << "VideoCapture error\n";
 	}
 
+    // 파일 열기
+    mode_fp = fopen(MODE_FILE_NAME, "r");
+    
 	while(1)
 	{
+        int mode;
 		struct timeval time;
 #ifdef DEBUG
  		gettimeofday(&time, NULL);
@@ -114,12 +123,12 @@ int main(int argc, char** argv)
 			return -1;
 		}
 
-		
-		mode_info[0] = '1';
-		mode_info[1] = '1';
-		mode_info[2] = '0';
-		
-		////////////subscribe contents and save to mode_info///////////////
+		// mode 읽기
+        fread(mode_info, 1, sizeof(mode_info), mode_fp);
+        fseek(mode_fp, 0, SEEK_SET);
+        mode = atoi(mode_info);
+        
+        // mode 정보 전송
 		ret = send(client_socket, mode_info, sizeof(mode_info), 0);
 		if(ret < 0) {
 			cerr << "mode info send fail\n";
@@ -130,13 +139,12 @@ int main(int argc, char** argv)
 		ret = recv(client_socket, recv_buff, sizeof(recv_buff), 0);
 
   		gettimeofday(&time, NULL);
-
 #ifdef DEBUG
   		double recv_time = (double)time.tv_sec + (double)time.tv_usec * .000001;
   		recv_time -= current_time;
   		cout << "time: " << recv_time << endl;
 #endif
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+
 
 #ifdef DEBUG
 		cout << "recv: " << recv_buff[0] << recv_buff[1] << recv_buff[2] << endl;
@@ -144,57 +152,92 @@ int main(int argc, char** argv)
 		struct tm tm = *localtime(&time.tv_sec);
 		char filename[256];
 
-		////////// not cordinated image ////////////////
-		// if (recv_buff[2] == '1') { // person img save
-		// 	sprintf(filename, "capture/person/%d%02d%02d_%02d%02d%02d.jpg", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-		// 	imwrite(filename, img, param_jpeg);
-		// }
-		// else if (recv_buff[2] == '2') { // fire img save
-		// 	sprintf(filename, "capture/fire/%d%02d%02d_%02d%02d%02d.jpg", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-		// 	imwrite(filename, img, param_jpeg);
-		// }
+        if (mode == 110) { // 감지모드 110
+            ////////// 박스 안친 이미지 저장 ////////////////
+            // if (recv_buff[2] == '1') { // person img save
+            // 	sprintf(filename, "~/capture/person/%d%02d%02d_%02d%02d%02d.jpg", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+            // 	imwrite(filename, img, param_jpeg);
+            // }
+            // else if (recv_buff[2] == '2') { // fire img save
+            // 	sprintf(filename, "~/capture/fire/%d%02d%02d_%02d%02d%02d.jpg", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+            // 	imwrite(filename, img, param_jpeg);
+            // }
 		
-		////////// cordinated image ////////////////
-		if (recv_buff[2] == '1' || recv_buff[2] == '2') {
-			int jpg_size = 0;
-			unsigned char* jpg_data = nullptr;
+            ////////// 박스 친 이미지 저장 ////////////////
+            if (recv_buff[2] == '1' || recv_buff[2] == '2') {
+                int jpg_size = 0;
+                unsigned char* jpg_data = nullptr;
 
-			if (recv_buff[2] == '1') { // person img save
-				sprintf(filename, "capture/person/%d%02d%02d_%02d%02d%02d.jpg", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-			}
-			else if (recv_buff[2] == '2') { // fire img save
-				sprintf(filename, "capture/fire/%d%02d%02d_%02d%02d%02d.jpg", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-			}
+                if (recv_buff[2] == '1') { // person img save name
+                    sprintf(filename, "~/capture/person/%d%02d%02d_%02d%02d%02d.jpg", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+                }
+                else if (recv_buff[2] == '2') { // fire img save name
+                    sprintf(filename, "~/capture/fire/%d%02d%02d_%02d%02d%02d.jpg", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+                }
 
-			// jpg size recv
-			ret = 0;
-			while (ret != sizeof(int)) {
-				ret += recv(client_socket, &jpg_size + ret, sizeof(int) - ret, 0);
-				if(ret < 0) {
-					cerr << "jpg info receive fail\n";
-					return -1;
-				}
-			}
-			jpg_data = (unsigned char*)calloc(jpg_size, sizeof(unsigned char));
+                // jpg size recv
+                ret = 0;
+                while (ret != sizeof(int)) {
+                    ret += recv(client_socket, &jpg_size + ret, sizeof(int) - ret, 0);
+                    if(ret < 0) {
+                        cerr << "jpg info receive fail\n";
+                        return -1;
+                    }
+                }
+                jpg_data = (unsigned char*)calloc(jpg_size, sizeof(unsigned char));
 
-			// jpg data recv
-			ret = 0;
-			while(ret != jpg_size){ // 받아야하는 값 만큼 실제 받을때까지,
-				ret += recv(client_socket, jpg_data + ret, jpg_size - ret, 0);
-				if(ret < 0) {
-					cerr << "jpg data receive fail\n";
-					return -1;
-				}
-			}
+                // jpg data recv
+                ret = 0;
+                while(ret != jpg_size){ // 받아야하는 값 만큼 실제 받을때까지,
+                    ret += recv(client_socket, jpg_data + ret, jpg_size - ret, 0);
+                    if(ret < 0) {
+                        cerr << "jpg data receive fail\n";
+                        return -1;
+                    }
+                }
 
-			// jpg decode & jpg save
-			vector<uchar> decoding(jpg_data, jpg_data + jpg_size);
-        	Mat jpg_img = imdecode(decoding, IMREAD_COLOR);
-			imwrite(filename, jpg_img, param_jpeg);
-		}
+                // jpg decode & jpg save
+                vector<uchar> decoding(jpg_data, jpg_data + jpg_size);
+                Mat jpg_img = imdecode(decoding, IMREAD_COLOR);
+                imwrite(filename, jpg_img, param_jpeg);
+            } // 이미지 저장 끝
 
-		////////////if(recv_buff[1] == 5) write the stop at mode.txt///////////
-		
+            //////////// 정지 명령 쓰기 /////////////
+            if (recv_buff[1] == '5') { // 정지
+                // 파일에 쓰기 또는 publish
+                mode110_fp = fopen(MODE110_FILE_NAME, "w");
+                // cerr << "mode310 write: " << recv_buff[1] << endl;
+                fwrite(recv_buff + 1, 1, 1, mode110_fp);
+                fseek(mode110_fp, 0, SEEK_SET);
+                fclose(mode110_fp);
+            }
+            else {// 정지아님
+                // 파일에 쓰기 또는 publish
+                mode110_fp = fopen(MODE110_FILE_NAME, "w");
+                // cerr << "mode310 write: " << recv_buff[1] << endl;
+                fwrite(recv_buff + 1, 1, 1, mode110_fp);
+                fseek(mode110_fp, 0, SEEK_SET);
+                fclose(mode110_fp);
+            }
+        } // 감지모드 110 끝
+        
+        if(mode == 310) { // 추적모드 310
+            // 파일에 recv_buff[1] 쓰기
+            mode310_fp = fopen(MODE310_FILE_NAME, "w");
+            // cerr << "mode310 write: " << recv_buff[1] << endl;
+            fwrite(recv_buff + 1, 1, 1, mode310_fp);
+            fseek(mode310_fp, 0, SEEK_SET);
+            fclose(mode310_fp);
+        } // 추적모드 310 끝
+        else { // 추적모드 310 이 아닌 경우
+            // 파일에 다른 값 쓰기
+            mode310_fp = fopen(MODE310_FILE_NAME, "w");
+            recv_buff[1] = '0';
+            // cerr << "mode310 write: " << recv_buff[1] << endl;
+            fwrite(recv_buff + 1, 1, 1, mode310_fp);
+            fseek(mode310_fp, 0, SEEK_SET);
+            fclose(mode310_fp);
+        }
 	}
 	
 	cerr << "Quit the program!\n";
@@ -203,6 +246,6 @@ int main(int argc, char** argv)
 #endif
 	//free(send_data);
 	close(client_socket);
-
+    fclose(mode_fp)
 	return 0;
 }
